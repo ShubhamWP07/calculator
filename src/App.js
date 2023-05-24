7405d0080f9dd51b43d76931048c4b62e73b6889
ShubhamWP07 <shubham1900384@gmail.com>
2023-05-24 19:04:10 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        setResult(eval(result).toString());
+      } catch (error) {
+        setResult("");
+      }
+    } else if (value === "AC") {
+      setResult("");
+    } else if (value === "PER") {
+      setResult((result / result) * 100);
+    } else if (value === "DEL") {
+      setResult(result.slice(0, -1));
+    } else {
+      setResult(result + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="calc-section">
+        <div className="input-box">
+          <input
+            type="text"
+            value={result}
+            className="calculator-screen"
+            disabled
+          />
+        </div>
+        <div>
+          <table className="calculator-buttons">
+            <tr>
+              <td>
+                <button
+                  className="calculator-button white"
+                  onClick={() => handleClick("AC")}
+                >
+                  AC
+                </button>
+                <button
+                  className="calculator-button white"
+                  onClick={() => handleClick("PER")}
+                >
+                  %
+                </button>
+                <button
+                  className="calculator-button white"
+                  onClick={() => handleClick("/")}
+                >
+                  <i className="fa-solid fa-divide"></i>
+                </button>
+                <button
+                  className="calculator-button orange"
+                  onClick={() => handleClick("DEL")}
+                >
+                  <i className="fa-solid fa-delete-left"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("7")}
+                >
+                  7
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("8")}
+                >
+                  8
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("9")}
+                >
+                  9
+                </button>
+                <button
+                  className="calculator-button orange"
+                  onClick={() => handleClick("*")}
+                >
+                  x
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("4")}
+                >
+                  4
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("5")}
+                >
+                  5
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("6")}
+                >
+                  6
+                </button>
+                <button
+                  className="calculator-button orange"
+                  onClick={() => handleClick("-")}
+                >
+                  -
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("1")}
+                >
+                  1
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("2")}
+                >
+                  2
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick("3")}
+                >
+                  3
+                </button>
+                <button
+                  className="calculator-button orange"
+                  onClick={() => handleClick("+")}
+                >
+                  +
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button
+                  className="calculator-button extra-width black"
+                  onClick={() => handleClick("0")}
+                >
+                  0
+                </button>
+                <button
+                  className="calculator-button black"
+                  onClick={() => handleClick(".")}
+                >
+                  .
+                </button>
+                <button
+                  className="calculator-button orange"
+                  onClick={() => handleClick("=")}
+                >
+                  =
+                </button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <p className="dev">Made by Shubham kumar</p>
     </div>
   );
-}
+};
 
 export default App;
